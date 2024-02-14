6f6ea0b8faec8015968467520331d7a7a3415794
Oleksandr Lebedko <oleksandrlebedko@Oleksandrs-MacBook-Pro.local>
2024-02-14 18:58:47 +0200

    initial commit

--- calculate_average.sh
+++ calculate_average.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 JAVA_OPTS="--enable-preview"
-./mvnw clean verify && java $JAVA_OPTS --class-path target/100mr-1.0-SNAPSHOT.jar ua.lebedko.Main
+./mvnw clean verify &&   /usr/bin/time java $JAVA_OPTS --class-path target/100mr-1.0-SNAPSHOT.jar ua.lebedko.Main
